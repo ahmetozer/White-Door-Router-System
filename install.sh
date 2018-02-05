e066558420477a53e9f3e4943572cfb3cd4cd049
Ahmet ÖZER <ultimatedev@outlook.com>
2018-02-06 01:36:11 +0300

    q

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-echo "tmpfs       /var/log/nginx tmpfs   nodev,nosuid,noexec,nodiratime,size=1M   0 0" >> /etc/fstab
-echo "tmpfs       /var/log/ tmpfs   nodev,nosuid,noexec,nodiratime,size=5M   0 0" >> /etc/fstab
+if [-f /drouter.readme]
+then
+  echo 'system already installed'
+else
+  #system backup
+  mkdir -p /drouter/backup
+  cp /etc/pam.d/ssh /drouter/backup/
+  cp /etc/ssh/sshd_config /drouter/backup/
+  cp /etc/fstab /drouter/backup
+  #Files installing
+  echo "tmpfs       /var/log/nginx tmpfs   nodev,nosuid,noexec,nodiratime,size=1M   0 0" >> /etc/fstab
+  echo "tmpfs       /var/log/ tmpfs   nodev,nosuid,noexec,nodiratime,size=5M   0 0" >> /etc/fstab
+  cp debian/* /
+  #Services restart
+  service ssh restart
