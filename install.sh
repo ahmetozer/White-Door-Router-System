57ed8d0e4aa53749c29196736ac410112217f85c
Ahmet OZER <iletisim@ahmetozer.org>
2018-08-28 14:28:28 +0300

    . Banana Pi SD Card Image

--- install.sh
+++ install.sh
@@ -1,8 +1,1 @@
-rm -rf  /var/log/*
-rm -rf  /run/log/*
-
-echo "tmpfs /var/log tmpfs defaults,noatime,nosuid,nodev,noexec,mode=1777,size=12M 0 0
-tmpfs /run/log tmpfs defaults,noatime,nosuid,nodev,noexec,mode=1777,size=12M 0 0
-tmpfs /var/tmp tmpfs defaults,noatime,nosuid,nodev,noexec,mode=1777,size=12M 0 0" >>  /etc/fstab
-
-apt install --no-install-recommends dnsmasq nodejs ca-certificates
+echo "Not available"
