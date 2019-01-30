5b9c0a567574184f0645f5368519051da20b6c12
Ahmet ÖZER <iletisim@ahmetozer.org>
2019-01-30 14:04:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
-echo "Not available"
+  set -e
+  set -x
+  echo "Not available"
 exit
 
 
 which apt || (echo please install git ; echo aa)
 which git || (echo please install git ; exit 1)
 
+useradd -m whitedoor
+adduser whitedoor sudo
+
+cd /home/whitedoor
+uname -a | grep Microsoft && wget https://github.com/AhmetOZER/White-Door-Router-System/raw/master/bash-on-windows-wsl-fix.sh
+chmod 755 bash-on-windows-wsl-fix.sh
+./bash-on-windows-wsl-fix.sh
 git clone git://git.buildroot.net/buildroot
